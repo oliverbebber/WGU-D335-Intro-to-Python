264ce06a2e46703e5a3969a774e02c2c043b586e
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-07-03 17:14:33 -0400

    Update challenge_activities.py
    Added 2.11.2 Q2 & Q3

--- lesson-02-variables-and-expressions/challenge_activities.py
+++ lesson-02-variables-and-expressions/challenge_activities.py
@@ -1,7 +1,32 @@
 # 2.11.2
+# q1 
 name1_input = input()
 
 my_str = "picked the \"freshest\" dates"
 
 # Output name1_input followed by my_str, separated by a space
 print(name1_input, my_str) 
+
+
+
+#2.11.2 
+# q2
+folder_input = input()
+
+# Output folder names on new line after accepting input
+computer_folders = "\nSystem\nAppData\nContacts\nBackup"
+
+print(folder_input, end="")
+print(computer_folders)
+
+
+
+#2.11.2
+# q3
+vegetable_input = input()
+
+# Output vegetables on a new line with indentations between each
+my_list = "Finn\'s vegetables:\nCorn\tPeas\t"
+
+print(my_list, end="")
+print(vegetable_input)
