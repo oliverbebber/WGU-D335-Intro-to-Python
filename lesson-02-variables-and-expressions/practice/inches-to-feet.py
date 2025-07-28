cdc032c7a9fff180468fc8c18cb0609e8b5c6ed8
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-07-28 16:49:15 -0400

    Update inches-to-feet.py

--- lesson-02-variables-and-expressions/practice/inches-to-feet.py
+++ lesson-02-variables-and-expressions/practice/inches-to-feet.py
@@ -1,1 +1,11 @@
+# Read total inches from user input
+total_inches = int(input())
 
+# Divide total inches by 12 to get the number of feet (12in = 1ft)
+feet = total_inches // 12 
+
+# Calculate remaining inches using modulus operator
+inches = total_inches % 12
+
+# Print the result in feet and inches format (e.g., 5'5")
+print(f"{feet}'{inches}\"")
