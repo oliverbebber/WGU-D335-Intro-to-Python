b2ed2b8a1edb4b76b981a10d503185a849543faa
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-07-28 17:07:41 -0400

    Update convert-to-seconds.py

--- lesson-02-variables-and-expressions/labs/convert-to-seconds.py
+++ lesson-02-variables-and-expressions/labs/convert-to-seconds.py
@@ -1,1 +1,9 @@
+seconds = int(input())
+minutes = int(input())
+hours = int(input())
 
+# Convert hours and minutes to seconds, then add together for total seconds
+total_seconds = seconds + (minutes * 60) + (hours * 3600)
+
+# Output total number of seconds
+print(f"{total_seconds} seconds")
