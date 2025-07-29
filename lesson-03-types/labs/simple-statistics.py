971eb14121b609f8602092073660099615dd0b62
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-07-29 16:08:31 -0400

    Update simple-statistics.py

--- lesson-03-types/labs/simple-statistics.py
+++ lesson-03-types/labs/simple-statistics.py
@@ -1,1 +1,17 @@
+# Given 4 floating-point numbers
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
 
+# Calculate product of 4 floating-point numbers
+product = num1 * num2 * num3 * num4
+
+# Calculate the average of 4 floating-point numbers
+average = (num1 + num2 + num3 + num4) / 4
+
+# Output each rounded integer 
+print(f'{product:.0f} {average:.0f}')
+
+# Output each floating-point value with 3 digits after the decimal point
+print(f'{product:.3f} {average:.3f}')
