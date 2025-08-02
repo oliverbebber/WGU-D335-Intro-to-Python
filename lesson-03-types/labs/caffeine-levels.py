ed250cb27e0e290bdc28bc55ff10eea11df658d9
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-02 19:00:59 -0400

    Update caffeine-levels.py

--- lesson-03-types/labs/caffeine-levels.py
+++ lesson-03-types/labs/caffeine-levels.py
@@ -1,1 +1,11 @@
+caffeine_mg = float(input())
 
+# Caffeine half life is about 6 hours
+after_six_hours = caffeine_mg / 2
+after_twelve_hours = after_six_hours / 2
+after_twentyfour_hours = after_twelve_hours / 4  # 2 more halves: 12 -> 18 -> 24
+
+# Output caffeine level after 6, 12, and 24 hours
+print(f'After 6 hours: {after_six_hours:.2f} mg')
+print(f'After 12 hours: {after_twelve_hours:.2f} mg')
+print(f'After 24 hours: {after_twentyfour_hours:.2f} mg')
