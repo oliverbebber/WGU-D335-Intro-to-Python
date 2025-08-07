0d0ebdea57a307c67b8629b5998d92938a4521aa
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:41:21 -0400

    Update volume-and-area-of-cylinder.py

--- lesson-02-variables-and-expressions/labs/volume-and-area-of-cylinder.py
+++ lesson-02-variables-and-expressions/labs/volume-and-area-of-cylinder.py
@@ -1,1 +1,15 @@
+import math
 
+r = float(input())
+h = float(input())
+
+pi = math.pi
+
+# Calculate volume for cubic inches
+cubic_inches = (pi * r**2) * h
+
+# Calculate area for square inches
+square_inches = (2 * pi * r * h) + (2 * pi * r * r)
+
+print(f'Volume (cubic inches): {cubic_inches:.2f}')
+print(f'Surface area (square inches): {square_inches:.2f}')
