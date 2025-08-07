14a84e358282ecb078d651cbcf06fde54ff52e33
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:18:20 -0400

    Update area-of-a-triangle.py

--- lesson-02-variables-and-expressions/labs/area-of-a-triangle.py
+++ lesson-02-variables-and-expressions/labs/area-of-a-triangle.py
@@ -1,1 +1,14 @@
+import math
 
+a = float(input())
+b = float(input())
+c = float(input())
+
+# half of the triangle's perimeter
+s = (a + b + c) / 2
+
+# area = sqrt of s * (s-a) * (s-b) * (s-c)
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))  # remember to use math module before sqrt function
+
+# output the floating-point value of the area with 2 digits after the decimal point
+print(f'Triangle area = {area:.2f}')
