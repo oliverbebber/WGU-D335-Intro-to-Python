ba6b5e73fbbe15436922ff2db7b3f516ff623717
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:13:10 -0400

    Update hypotenuse.py

--- lesson-02-variables-and-expressions/labs/hypotenuse.py
+++ lesson-02-variables-and-expressions/labs/hypotenuse.py
@@ -1,1 +1,10 @@
+a = float(input())
+b = float(input())
 
+print(f'Right triangle has side lengths {a:.2f} and {b:.2f}')
+
+# Calculate hypotenuse using Pythagorean's Theorem
+hypotenuse = (a**2 + b**2) ** 0.5
+
+# Output the length of the third side (hypotenuse) with 2 digits after the decimal point
+print(f'Hypotenuse is {hypotenuse:.2f}')
