1c87f0209d7acbe3e3755691dbd7dd3c7f8a0bd1
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:56:50 -0400

    Update fixing_errors_in_kite.py

--- lesson-01-intro/labs/fixing_errors_in_kite.py
+++ lesson-01-intro/labs/fixing_errors_in_kite.py
@@ -1,1 +1,19 @@
+int long_side = 12
+int short_side = 10
+int perimeter
 
+perimeter = 2 * long_side + * short_side
+
+printl("longSide= ", long_side)
+printl("shortSide= ", short_side)
+print("perimeter = : " * perimeter)
+
+# Resolving errors
+long_side = 12
+short_side = 10
+
+perimeter = 2 * (long_side + short_side)
+
+print(f'Long side = {long_side}')
+print(f'Short side = {short_side}')
+print(f'Perimeter = {perimeter}')
