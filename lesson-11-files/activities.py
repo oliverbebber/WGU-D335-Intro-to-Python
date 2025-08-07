84d61e5418a626b990448550c50f3e5d2403a454
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 17:33:17 -0400

    Add 11.6.3 Challenges

--- lesson-11-files/activities.py
+++ lesson-11-files/activities.py
@@ -1,1 +1,11 @@
+# 11.6.3
+import os
+p = 'C:\\Programs\\Microsoft\\msword.exe'
+print(os.path.split(p))
+# Output: ('C:\\Programs\\Microsoft', 'msword.exe')
 
+os.path.isfile('C:\\Program Files\\')
+# Output: False
+
+os.path.getsize(path_str)
+# returns the size in bytes of the file at path_str
