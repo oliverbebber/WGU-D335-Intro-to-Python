5b1f7239d91b52cb252665fa7f5c48252e0069f5
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 16:58:16 -0400

    Add 3.9.2 Activities

--- lesson-03-types/activities.py
+++ lesson-03-types/activities.py
@@ -7,3 +7,15 @@ num_items + num_items   # 10
 item_weight * num_items  # 2.5
 
 (num_items + num_items) * item_weight  # 5.0
+
+
+
+# 3.9.2
+int(1.55)
+# truncates to 1
+
+float("7.99")
+# converts directly to the floating point 7.99
+
+str(99)
+# converts 99 to a string
