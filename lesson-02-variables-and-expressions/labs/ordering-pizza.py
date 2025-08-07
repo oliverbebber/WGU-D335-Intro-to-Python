7420f06b2a0fd05d83ceec024e812f56708f30ca
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:25:46 -0400

    Update ordering-pizza.py

--- lesson-02-variables-and-expressions/labs/ordering-pizza.py
+++ lesson-02-variables-and-expressions/labs/ordering-pizza.py
@@ -1,1 +1,13 @@
+num_pizzas = int(input())
+cost = 14.99
+tax_rate = 0.08
 
+# calculate subtotal
+subtotal = (num_pizzas * cost)
+
+# calculate total cost
+total_cost = subtotal + (subtotal * tax_rate)
+
+print(f'Pizzas: {num_pizzas}')
+print(f'Subtotal: ${subtotal:.2f}')
+print(f'Total due: ${total_cost:.2f}')
