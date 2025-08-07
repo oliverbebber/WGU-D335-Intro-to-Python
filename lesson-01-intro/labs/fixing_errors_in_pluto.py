1885a794fad0a683ec3312d4ade3190329d7d0a3
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 19:51:26 -0400

    Update fixing_errors_in_pluto.py

--- lesson-01-intro/labs/fixing_errors_in_pluto.py
+++ lesson-01-intro/labs/fixing_errors_in_pluto.py
@@ -1,1 +1,15 @@
+int proportion = 6
 
+print("Is Pluto a planet?")
+print("Some people think so, ")
+print("but others don't.")
+print("The Moon's mass is " + proportion + " times Pluto's.")  # first syntax error
+print("Not much of a planet, is it.")
+
+# Resolved errors
+proportion = 6
+
+print("Is Pluto a planet?")
+print("Some people think so, but others don't.")
+print(f'The Moon\'s mass is {proportion} times Pluto\'s.')
+print("Not much of a planet, is it?")
