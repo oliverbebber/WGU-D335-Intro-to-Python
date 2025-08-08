31884c653dcc74f9eb2e8e95311e87cd945d5715
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-07 20:05:20 -0400

    Update fixing_errors_in_baseball.py

--- lesson-01-intro/labs/fixing_errors_in_baseball.py
+++ lesson-01-intro/labs/fixing_errors_in_baseball.py
@@ -1,1 +1,19 @@
+dodgers-score = input()
+yankees-score = input()
+cubs-score = input()
 
+print("Last night tThe Dodgers scored ", dodgers-score)
+print("The Cubs scored ", orioles_score)
+print("The Yankees scored ", yankees-score)
+print("The total number of runs scored was", dodgers-score + yankees-score + orioles_score)
+
+# Resolving errors
+dodgers_score = int(input())
+yankees_score = int(input())
+cubs_score = int(input())
+
+print(f'Last night the Dodgers scored {dodgers_score}')
+print(f'The Yankees scored {yankees_score}')
+print(f'The Cubs scored {cubs_score}\n')
+
+print(f'The total number of runs scored was {dodgers_score + yankees_score + cubs_score}')
