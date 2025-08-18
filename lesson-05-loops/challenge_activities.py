e4b6be8e880fdc5deae863f2f710fade5823aeee
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-18 14:07:21 -0400

    Add 5.2.1 Challenge Activities

--- lesson-05-loops/challenge_activities.py
+++ lesson-05-loops/challenge_activities.py
@@ -1,1 +1,62 @@
+# 5.2.1
+g = 0
 
+while g <= 2:
+  print(g)
+  g += 1
+
+# output: 
+# 0
+# 1
+# 2
+#
+
+g = 0
+
+while g <=5:
+  print(g)
+  g += 2
+
+# output:
+# 0
+# 2
+# 4
+#
+
+g = 0
+
+while g >= -3:
+  print(g)
+  g -= 1
+
+# output:
+# 0
+# -1
+# -2
+# -3
+#
+
+g = 0
+
+while g >= -4:
+  print(g)
+  g -= 2
+
+# output
+# 0
+# -2
+# -4
+#
+
+g = 4
+
+while g <= 7:
+  print(g)
+  g += 1
+
+# output:
+# 4
+# 5
+# 6
+# 7
+# 
