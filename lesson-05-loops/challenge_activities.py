6ea180438fc5301d08351c5562727c2f92e2d2cf
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-18 14:35:55 -0400

    Add 5.2.2 Challenges

--- lesson-05-loops/challenge_activities.py
+++ lesson-05-loops/challenge_activities.py
@@ -60,3 +60,44 @@ while g <= 7:
 # 6
 # 7
 # 
+
+
+
+
+
+# 5.2.2
+input_val = float(input())
+while input_val < 1.0:  # executes while loop when input_val is less than -1.0
+    print(f'User entered {input_val}')
+    input_val = float(input())
+
+print('Loop terminated')
+
+
+
+
+sum_chars = 0
+char_input = input()
+
+while char_input != 's':  # executes while loop without reading 's'
+  sum_chars += 1  # updates sum_chars with the sum of sum_chars +1
+  char_input = input()  # read next character from input into char_input
+  
+print(sum_chars)
+
+
+
+
+result = 0
+user_num = int(input())
+
+while user_num >= 0:  # while user_num is non-negative
+  if user_num % 4 != 0:  # if user_num is not divisible by 4
+    result += user_num # add user_num to result
+  else:  # otherwise
+    result -= user_num  # subtract user_num from result
+    
+  user_num = int(input())  # read next int from input
+    
+print(f'Result is {result}')
+
