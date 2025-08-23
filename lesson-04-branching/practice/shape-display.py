56d22559ea1a9bd44b0e6fa5bd2259c6cd461faa
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-23 16:13:58 -0400

    Add solution with notes

--- lesson-04-branching/practice/shape-display.py
+++ lesson-04-branching/practice/shape-display.py
@@ -1,1 +1,10 @@
+shape_name = input()
 
+if shape_name == "square":
+    print("***")
+    print("* *")
+    print("***")
+elif shape_name == "triangle":
+    print("  *")
+    print(" * *")   # no trailing whitespace
+    print("*****")  # no trailing whitespace
