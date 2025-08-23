adccf894019683b03a5c89f56a39753b1743c415
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-23 13:07:34 -0400

    Add solution

--- lesson-02-variables-and-expressions/labs/using-math-functions.py
+++ lesson-02-variables-and-expressions/labs/using-math-functions.py
@@ -1,1 +1,21 @@
+import math
 
+# given three floating-point numbers x, y, and z
+x = float(input())
+y = float(input())
+z = float(input())
+
+# calculate x to the power of z
+xpowz = x ** z
+
+# calculate x to the power of (y to the power of z)
+xpow_ypowz = x ** (y ** z)
+
+# calculate the absolute value of (x - y)
+abs_xy = abs(x - y)
+
+# calculate the square root of (x to the power of z)
+sqrt_xpowz = math.sqrt(x ** z)
+
+# output x to the power of z, x to the power of (y to the power of z), the absolute value of (x - y), and the square root of (x to the power of z)
+print(f'{xpowz:.2f} {xpow_ypowz:.2f} {abs_xy:.2f} {sqrt_xpowz:.2f}')
