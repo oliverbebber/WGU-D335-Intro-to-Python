0133b6195cd634a57aa2a92d2f03a5b04ae85f48
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-23 16:39:11 -0400

    Add solution with notes

--- lesson-04-branching/labs/max-of-two.py
+++ lesson-04-branching/labs/max-of-two.py
@@ -1,1 +1,16 @@
+int1 = int(input())
+int2 = int(input())
 
+# determine max value
+max_val = max(int1, int2)
+
+# if int1 is greater than int2
+if int1 > int2:
+  # output max_val
+  print(f'Max of {int1} and {int2} is {max_val}')
+# elif int2 > int1:
+#  print(f'Max of {int2} and {int1} is {max_val}')
+else:  # int1 == int2
+  print(f'Max of {int1} and {int2} is {max_val}')
+
+# unsure if an elif statement is needed as the lab was passed with AND without it
