3e70b9f83337a8d34782cc690c6d7645bd3f266d
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-23 19:22:58 -0400

    Add 8.1.3 Activities

--- lesson-08-lists-and-dictionaries/activities.py
+++ lesson-08-lists-and-dictionaries/activities.py
@@ -1,1 +1,15 @@
+# 8.1.3
+animals = ['cat', 'dog', 'bird', 'raptor']
+print(animals[0])
+# cat
 
+print(animals[2])
+# bird
+
+print(animals[0 + 1])
+# 0 + 1 = 1
+# dog
+
+i = 3
+print(animals[i])
+# raptor
