fa834c80abdad6cfa7aa517f09886e1c77b5ee67
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-24 15:14:23 -0400

    Add solution

--- practice-test-1/convert-to-dollars.py
+++ practice-test-1/convert-to-dollars.py
@@ -1,1 +1,10 @@
+# given three input values representing nickels, dimes, and quarters
+nickels = int(input())
+dimes = int(input())
+quarters = int(input())
 
+# output the total amount as dollars and cents
+dollars = (nickels * 0.05) + (dimes * 0.10) + (quarters * 0.25)
+
+# output each floating-point value with two digits after the decimal point
+print(f'Amount: ${dollars:.2f}')
