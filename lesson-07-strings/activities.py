af959ec73e204d7286fe13cd912c7763896572af
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-23 20:42:22 -0400

    Add 7.1.4 Activities

--- lesson-07-strings/activities.py
+++ lesson-07-strings/activities.py
@@ -1,1 +1,9 @@
+# 7.1.4
+my_str = 'http://reddit.com/r/python'
+print(my_str[17:])  # output: /r/python
 
+my_str = 'http://reddit.com/r/python'
+protocol = 'http://'
+print(my_str[len(protocol):])  # len(protocol) = 7    my_str[7:]
+
+# output: reddit.com/r/python
