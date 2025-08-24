3718e05b276a59dda18788f463de7f2ed0955a8e
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-24 17:26:41 -0400

    Update with comments to guide solution

--- lesson-05-loops/labs/months-until-payoff.py
+++ lesson-05-loops/labs/months-until-payoff.py
@@ -1,1 +1,15 @@
+# write a program that reads a loan amount, payment amount, and interest rate as inputs
+loan_amount = float(input())  # aka current balance
+payment_amount = float(input())
+interest_rate = float(input())
 
+# initialize a counter to track how many payments are made
+# initialize the current balance on the loan
+
+# loop until balance is less than or equal to zero:
+  # first, apply interest to the current balance
+  # then, subtract the payment amount from the balance
+  # increase the payment counter by 1 (+= 1)
+
+# after loop ends, output the number of payments remaining
+# be sure output language aligns for singular or plural
