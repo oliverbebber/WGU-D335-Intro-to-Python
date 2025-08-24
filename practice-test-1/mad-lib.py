37b6c37154eb2ef24d8dbb0b27a043ce6572d35b
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-24 15:11:09 -0400

    Add solution

--- practice-test-1/mad-lib.py
+++ practice-test-1/mad-lib.py
@@ -1,1 +1,14 @@
+# create a program that reads four values from input
+# store as first_name, whole_number, plural_noun, and generic_location
+# this program will use the input values to output a short story
 
+# Read a value from a user and store the value in first_name
+first_name = input()
+
+# TODO: Type your code to read three more values here.
+whole_number = int(input())
+plural_noun = input()
+generic_location = input()
+
+# Output a short story using the four input values. Do not modify the code below.
+print(f'{first_name} buys {whole_number} different types of {plural_noun} at {generic_location}')
