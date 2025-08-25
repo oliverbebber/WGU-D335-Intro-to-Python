a78e317e74fe19f84102a50baac73f621f01b204
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-25 13:49:36 -0400

    Add 7.2.1 Challenge Activities: Field widths

--- lesson-07-strings/challenge-activities.py
+++ lesson-07-strings/challenge-activities.py
@@ -1,1 +1,21 @@
+# 7.2.1
+name = 'Sadio'
+print(f'{name:8}')  # total characters, including white space is 8
 
+# output: Sadio   
+# 
+
+name = 'Jack'
+points = 11
+print(f'{name:6}{points:5}')  # points:5 will include white space = 3 then the remaining 2 would be 11
+
+# output: Jack     11
+#
+
+name = 'Jane'
+matches = 21
+points = 8
+print(f'{name:6}{points:3}{matches:5}')  # matches:5 will be similar to points:5 - white space = 3 then the remaining 2 will be 21
+
+# output: Jane    8   21
+# 
