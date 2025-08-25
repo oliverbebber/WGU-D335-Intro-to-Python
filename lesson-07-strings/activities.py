8533ce6656173f778e6355366e075fd48cd1d9a5
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-25 13:44:21 -0400

    Add 7.2.2 Activities: Format specification field widths

--- lesson-07-strings/activities.py
+++ lesson-07-strings/activities.py
@@ -10,7 +10,7 @@ print(my_str[len(protocol):])  # len(protocol) = 7    my_str[7:]
 
 
 
-# 8.1.5
+# 7.1.5
 my_str = 'Agt2t3afc2kjMhagrds!'
 print(my_str[0:5:1])  
 
@@ -21,3 +21,11 @@ my_str = 'Agt2t3afc2kjMhagrds!'
 print(my_str[::2])
 
 # output: AttackMars
+
+
+# 7.2.2
+# complete the format specification to assign a field width of 10 chars
+{name:10}
+
+# write a complete replacement field that assigns a field with named value "count" and a field width of 5
+{count:5}
