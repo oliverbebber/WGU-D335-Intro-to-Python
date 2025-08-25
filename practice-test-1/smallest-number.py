7f435d9c33801f714ee8ce8760d28e4fbcbfe2cb
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-25 13:28:39 -0400

    Add solution

--- practice-test-1/smallest-number.py
+++ practice-test-1/smallest-number.py
@@ -1,1 +1,10 @@
+# write a program that takes three int as input
+a = int(input())
+b = int(input())
+c = int(input())
 
+# determine min value
+min_val = min(a, b, c)
+
+# output the smallest of the three values
+print(min_val)
