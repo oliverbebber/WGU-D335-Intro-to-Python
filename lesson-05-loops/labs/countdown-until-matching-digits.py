913c0dce0b6a12e9174bbf94f1c55c2f4f6a8a27
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-27 15:11:24 -0400

    Add solution with notes

--- lesson-05-loops/labs/countdown-until-matching-digits.py
+++ lesson-05-loops/labs/countdown-until-matching-digits.py
@@ -1,1 +1,21 @@
+# read integer from input
+num = int(input())
 
+# check if input is out of valid range (11-99)
+if num < 11 or num > 99:
+  print('Input must be 11-99')
+else:
+  # loop until both digits are the same
+  while True:
+    # print the current number in the countdown
+    print(num)
+    # determine the tens and ones digits
+    tens = num // 10  # int division provides the tens place
+    ones = num % 10  # modulo gives the ones place
+    
+    # if both digits are the same, stop the loop
+    if tens == ones:
+      break
+      
+    # otherwise, continue conuntdown
+    num -= 1
