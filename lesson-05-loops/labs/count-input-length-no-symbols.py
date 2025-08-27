0b01f290a26c5f5ebbb923f136699502c53c60be
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-08-27 15:03:18 -0400

    Add solution with notes

--- lesson-05-loops/labs/count-input-length-no-symbols.py
+++ lesson-05-loops/labs/count-input-length-no-symbols.py
@@ -1,1 +1,13 @@
+# given a line of text as input
+user_text = input()
 
+# def count as starting at 0
+count = 0
+
+# iterate through each character in user_text
+for n in user_text:
+    if n not in [' ', '.', '!', ',']:  # skip excluded characters
+        count += 1
+
+# output the number of characters excluding spaces, periods, commas, or exclamation points.
+print(count)
