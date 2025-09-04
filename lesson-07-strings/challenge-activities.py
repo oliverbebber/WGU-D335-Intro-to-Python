e97300e0ed0588f1f2e8a99604045080d0af62be
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-04 16:09:02 -0400

    Add 7.1.1 Challenge Activities: Output of sliced strings

--- lesson-07-strings/challenge-activities.py
+++ lesson-07-strings/challenge-activities.py
@@ -1,3 +1,73 @@
+# 7.1.1
+city = 'Khartoum'
+city_slice = city[0:3]
+print(city_slice)
+# output: Kha
+#
+
+color = 'verdant'
+color_slice = color[:4]
+print(color_slice)
+# output: verd
+#
+
+city = 'Beirut'
+city_slice = city[:2]
+print(city_slice)
+# output: Be
+#
+
+state = 'Wyoming'
+my_slice = state[4:6]
+print(my_slice)
+# output: in
+# 
+
+color = 'violet'
+my_slice = color[4:6]
+print(my_slice)
+# output: et
+# 
+
+city = 'Quito'
+my_slice = city[0:4:2] # yields every second element of state from indices 0 to 4
+print(my_slice)
+# output: Qi
+# 
+
+state = 'Vermont'
+my_slice = state[0:4:3]  # yields every third element of state from indices 0 to 3
+print(my_slice)
+# output: Ver
+# 
+
+color = 'silver'
+my_slice = color[0:4:3]  # yields every third element of state from indices 0 to 3
+print(my_slice)
+# output: sv
+# 
+
+city = 'Algiers'
+my_slice = city[0:5:2]  # yields every second element of city from indices 0 to 4
+print(my_slice)
+# output: Age
+#
+
+state = 'Idaho'
+my_slice = state[0:-2]  # yields the chars from indices 0 to -2; -2 refers to the second-from-last char 'h' at index 3
+print(my_slice)
+# output: Ida
+#
+
+city = 'Cairo'
+my_slice = city[0:-2]  # yields the chars from indices 0 to -2; -2 refers to the second-from-last char 'r' at index 3
+print(my_slice)
+# output: Cai
+# 
+
+
+
+
 # 7.2.1
 name = 'Sadio'
 print(f'{name:8}')  # total characters, including white space is 8
