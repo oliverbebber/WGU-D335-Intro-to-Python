0cd11c408e8898c7471159269fc2d1d066a95f68
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-05 15:21:25 -0400

    Add comments to solve later

--- lesson-10-modules/labs/radioactive-decay.py
+++ lesson-10-modules/labs/radioactive-decay.py
@@ -1,1 +1,35 @@
+# formula to calculate how much radioactive isotope with 
+# half-life T will remain after time t is:
+# N_{t}=N_{0}e^{\frac{-(\ln 2)t}{T}}
+
+# formula can be rearranged to calculate the half-life of isotope 
+# given how much remains after decay:
+# T=\frac{-(\ln 2)t}{\ln{\frac{N_{t}}{N_{0}}}}
+
+import math
+
+# An isotope has half-life T.
+# Return the amount of a starting mass N0
+# of the isotope that remains after time t,
+def compute_Nt(N0, t, T):
+    # Type your code here.
+
+# Return the half-life of an isotope given that
+# a mass N0 decays to Nt after time t
+def compute_T(N0, Nt, t):
+    # Type your code here.
+
+if __name__ == '__main__':
+    choice = input()
+
+    if choice == 'N':  # Calculate amount of material
+        # TODO: Read N0, t, and T from one line of input and compute Nt
+        
+        print(f'Nt = {Nt:.4f}')
+    elif choice == 'T':  # Calculate half-life
+        # TODO: Read N0, Nt, and t from one line of input and compute T
+        
+        print(f'T = {T:.4f}')
+    else:
+        print("invalid choice")
 
