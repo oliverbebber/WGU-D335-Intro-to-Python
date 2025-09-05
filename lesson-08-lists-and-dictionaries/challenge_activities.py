8929a61253b55d55740ce44add8a33de885a443e
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-05 15:14:48 -0400

    Add 8.2.1 Challenge Activity

--- lesson-08-lists-and-dictionaries/challenge_activities.py
+++ lesson-08-lists-and-dictionaries/challenge_activities.py
@@ -34,3 +34,19 @@ user_values[2] = user_values[1]
 print(user_values)
 # output: [7, 6, 6]
 # 
+
+
+
+
+# 8.2.1
+# int num_students is read from input
+num_students = int(input())
+
+students_list = []
+
+# read the remaining strings from input and insert each string at the front of students_list at position 0
+for i in range(num_students):
+  student = input()
+  students_list.insert(0, student)
+
+print(students_list)
