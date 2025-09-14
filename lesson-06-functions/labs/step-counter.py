aa33ae0630ee211d58ff97b19ae8fcd7fa3530fb
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-14 19:18:24 -0400

    Add solution with notes

--- lesson-06-functions/labs/step-counter.py
+++ lesson-06-functions/labs/step-counter.py
@@ -1,1 +1,12 @@
+# Define your function here 
+def feet_to_steps(user_feet):
+    # use float division, cast to int
+    result = int(user_feet / 2.5)
+    return result
 
+if __name__ == '__main__':
+    # Type your code here.
+    user_feet = float(input())
+    # call function and store result
+    steps = feet_to_steps(user_feet)
+    print(steps)
