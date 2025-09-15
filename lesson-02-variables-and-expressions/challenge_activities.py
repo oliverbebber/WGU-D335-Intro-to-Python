39e44836b80410800f24cd37acdc19d8ea833336
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-15 12:49:21 -0400

    Add 2.9.3 Challenges: Writing math calculations

--- lesson-02-variables-and-expressions/challenge_activities.py
+++ lesson-02-variables-and-expressions/challenge_activities.py
@@ -15,6 +15,53 @@ print('Decades:', num_decades)  # 1
 print('Years:', num_years)  # 8
 
 
+# 2.9.3
+# compute: answer = h - |i|
+import math
+
+h = float(input())  # 5.0
+i = float(input())  # 4.0
+
+answer = h - math.fabs(i)  # pipe isn't valid syntax, remember it stands for absolute value of i in this instance; hence the reason we imported math
+
+print(f'{answer:.1f}')  # Outputs answer (1.0) with 1 decimal place
+
+
+
+# computer: val = |g| * sqrt(h)
+import math
+
+g = float(input())  # 2.0
+h = float(input())  # 4.0
+
+val = math.fabs(g) * math.sqrt(h)  # don't forget to use dot notation on ALL pieces that came from importing math
+
+print(f'{val:.2f}')  # Outputs val (2.00) with 2 decimal places
+
+
+# compute: answer = (sqrt(p)) ** (sqrt(q))  
+import math
+
+p = float(input())  # 4.0
+q = float(input())  # 9.0
+
+answer = math.sqrt(p) ** (math.sqrt(q))  # remember, ^ is NOT for exponents but for bitwise XOR in Python
+
+print(f'answer = {answer:.2f}')  # Outputs answer (8.00) with 2 decimal places
+
+
+# calculate: k = (i - j)!/i!
+import math
+
+i = int(input())  # 5
+j = int(input())  # -3
+
+k = math.factorial(i - j) / math.factorial(i)  # factorial(x)
+
+print(f'k = {k:.2f}')  # Outputs k (336.00) with 2 decimal places.
+
+
+
 # 2.11.2
 # q1 
 name1_input = input()
