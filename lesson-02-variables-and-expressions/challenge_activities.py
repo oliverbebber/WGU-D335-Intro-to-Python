24d567095b0b23e85c7f4e9a20d35dd3ab5b942d
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-15 12:30:07 -0400

    Add 2.7.2 Division and Modulo Challenge

--- lesson-02-variables-and-expressions/challenge_activities.py
+++ lesson-02-variables-and-expressions/challenge_activities.py
@@ -1,3 +1,20 @@
+# 2.7.2 
+# convert total_years to millennia, decades, and years
+# finding the max number of millenia, then decades, then years
+total_years = int(input())  # input is 8018
+
+num_millennia = total_years // 1000  # 8018 // 1000 = 8 --> integer (floor) division
+remaining_years_after_millennia = total_years % 1000  # 8018 % 1000 = 8 --> 8 * 1000 = 8000 --> 8000 + x = 8018 --> x = 18, modulo 18
+
+num_decades = remaining_years_after_millennia // 10  # 18 // 10 = 1; num_decades is 1, num_years is the remainder; use modulo to solve
+num_years = remaining_years_after_millennia % 10  # 18 % 10 = 1 --> 1 * 10 = 10 --> 10 + x = 18 --> x = 8, modulo 8 
+
+# output
+print('Millennia:', num_millennia)  # 8
+print('Decades:', num_decades)  # 1
+print('Years:', num_years)  # 8
+
+
 # 2.11.2
 # q1 
 name1_input = input()
