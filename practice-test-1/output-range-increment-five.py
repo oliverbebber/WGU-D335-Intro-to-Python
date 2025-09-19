377cd57326c13630236c40ee067a173a0d408e40
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-19 11:51:39 -0400

    Add solution with notes

--- practice-test-1/output-range-increment-five.py
+++ practice-test-1/output-range-increment-five.py
@@ -1,1 +1,12 @@
+a = int(input())
+b = int(input())
 
+# output the first int and subsequent increments of 5 
+# if value is less than or equal to the second integer
+if a <= b:
+  for i in range(a, b + 1, 5):
+    print(i, end=' ')     # end=' ' prints output with space in between each i
+    print()   # end output with a new line
+else:
+  # output "Second integer can't be less than the first."
+  print('Second integer can\'t be less than the first.')
