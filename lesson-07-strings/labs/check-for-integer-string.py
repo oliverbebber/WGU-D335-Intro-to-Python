5937749c0d7abfc8190361ccb655302f90d20589
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-19 13:30:04 -0400

    Add solution

--- lesson-07-strings/labs/check-for-integer-string.py
+++ lesson-07-strings/labs/check-for-integer-string.py
@@ -1,1 +1,10 @@
+# write a program that accepts a string representing an integer as input
+user_string = input()
 
+# outputs "Yes" if the char is a digit 0-9
+if user_string.isdigit():  # .isdigit() returns Trye if all chars are numbers 0-9
+    print('Yes')
+# otherwise
+else:
+# outputs "No"
+    print('No')
