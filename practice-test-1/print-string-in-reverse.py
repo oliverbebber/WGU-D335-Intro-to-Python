5a361e2dffd3782150aa2aa8378497bef70991a3
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-21 12:51:20 -0400

    Add solution: 2 options

--- practice-test-1/print-string-in-reverse.py
+++ practice-test-1/print-string-in-reverse.py
@@ -1,1 +1,14 @@
+# write a program to take line of text as input
+while True:
+  text = input()
+  if text in ('Done', 'done', 'd'):   # stop condition
+    break
+    print(text[::-1])  # output the text in reverse
 
+
+
+# the solution could also be the following
+text = input()
+while text not in ('Done', 'done', 'd'):  # stop condition
+  print(text[::-1])  # output the text in reverse
+  text = input()
