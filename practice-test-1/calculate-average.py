2f5b88c8a8eaf5e9cc8caa0574ac5ed91e814780
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-21 13:30:25 -0400

    Add solution

--- practice-test-1/calculate-average.py
+++ practice-test-1/calculate-average.py
@@ -1,9 +1,9 @@
 def calc_average(nums):
   # complete the function that has an int list param
-  
+  average = sum(nums) / len(nums)
   # returns the average value of elements in the list as a float
-  
+  return average
     
 if __name__ == '__main__':
-    nums = [1, 2, 3, 4, 5]
-    print(calc_average(nums))   # calc_average() should return 3.0
+  nums = [1, 2, 3, 4, 5]
+  print(calc_average(nums))   # calc_average() should return 3.0
