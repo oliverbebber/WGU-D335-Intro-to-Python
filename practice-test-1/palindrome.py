cffaea0d60a12778da1dc965b4ea794691a10b6a
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-24 17:30:19 -0400

    Add solution with notes

--- practice-test-1/palindrome.py
+++ practice-test-1/palindrome.py
@@ -1,1 +1,16 @@
+# a palindrome is a word or phrase that is the same read forward and backward
+# ex: "bob", "sees", "never odd or even" (ignores spaces)
 
+# write a program whose input is a word or phrase
+# outputs whether the input is a palindrome
+# start by removing spaces
+# then check if the string equals itself in reverse
+word = input()
+
+# remove spaces
+remove_spaces = word.replace(' ', '')
+
+if remove_spaces == remove_spaces[::-1]:  # [::-1] reverses the entire string; [:-1] slices the last char
+    print(f'palindrome: {word}')
+else:
+    print(f'not a palindrome: {word}')
