bd97d56bf61a18f39b7b1026d68c63fa9e02cd2c
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-24 16:04:55 -0400

    Add solution

--- lesson-06-functions/labs/swapping-variables.py
+++ lesson-06-functions/labs/swapping-variables.py
@@ -1,1 +1,13 @@
+# Define your function here.
+def swap_values(user_val1, user_val2, user_val3, user_val4):
+    return user_val2, user_val1, user_val4, user_val3
 
+if __name__ == '__main__': 
+    # Type your code here. Your code must call the function.
+    user_val1 = int(input())
+    user_val2 = int(input())
+    user_val3 = int(input())
+    user_val4 = int(input())
+    
+    val1, val2, val3, val4 = swap_values(user_val1, user_val2, user_val3, user_val4)
+    print(val1, val2, val3, val4)
