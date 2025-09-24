90c083a6b2c11a55e584dde6ae8ec3f774ead83b
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-24 15:06:30 -0400

    Add 6.9.1 Challenge Activity: Function errors: Copying one function to create another

--- lesson-06-functions/challenge_activities.py
+++ lesson-06-functions/challenge_activities.py
@@ -143,3 +143,33 @@ print('Testing static input: ')
 output_sum(4, 6)
 print(f'\nTesting user input: ')
 output_sum(numberA, numberB)
+
+
+
+
+
+
+
+# 6.9.1
+def celsius_to_kelvin(value_celsius):
+    value_kelvin = 0.0
+
+    value_kelvin = value_celsius + 273.15
+    return value_kelvin
+
+# using the celsisus_to_kelvin function
+# create a new function kelvin_to_celsius
+# modify the function accordingly
+def kelvin_to_celsius(value_kelvin):
+    value_celsius = 0.0
+    
+    value_celsius = value_kelvin - 273.15
+    return value_celsius
+    
+value_c = 10.0
+print(f'{value_c} C is {celsius_to_kelvin(value_c)} K')
+
+value_k = float(input())
+print(f'{value_k} K is {kelvin_to_celsius(value_k)} C')
+
+
