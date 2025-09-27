63b89ef34fabd7b4a3115a90e2e4e52709a5254d
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-27 01:51:59 -0400

    Add solution

--- practice-test-2/lab-8.py
+++ practice-test-2/lab-8.py
@@ -1,1 +1,12 @@
+# given a string representing a 10-digit phone number
+# output the area code, prefix, and line number
+# following the format (800) 555-1212
+phone_number = str(input())
 
+phone_str = str(phone_number).strip()
+
+area_code = phone_str[0:3]
+prefix = phone_str[3:6]
+suffix = phone_str[6:]
+
+print(f'({area_code}) {prefix}-{suffix}')
