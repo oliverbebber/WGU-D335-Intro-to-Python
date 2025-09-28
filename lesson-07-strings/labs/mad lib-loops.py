8b269faf42330cac64896d936cd865ba5e382d2e
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-28 17:40:34 -0400

    Add solution with notes

--- lesson-07-strings/labs/mad lib-loops.py
+++ lesson-07-strings/labs/mad lib-loops.py
@@ -1,1 +1,17 @@
+# write a program that takes a string and int as input
+# outputs a sentence using the input values as shown in the following example
+    # Eating {num} {word} a day keeps you happy and healthy.
+# the program repeats until the input string is quit and disregards the int input after
 
+while True:
+  user_input = input().strip()        # format of user_input: apples 5
+
+  # split into word and number
+  word, num = user_input.split()  
+  # print(word, num)                  # returns apples 5
+  
+  # check for quit condition
+  if word == 'quit':
+    exit()
+  
+  print(f'Eating {num} {word} a day keeps you happy and healthy.')
