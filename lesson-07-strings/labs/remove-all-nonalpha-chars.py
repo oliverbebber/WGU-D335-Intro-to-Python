eb99e6d8267548126c0970637a4d80ad9799d3ac
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-28 19:01:02 -0400

    Add solution w/two options - refactored for simplifying code with notes

--- lesson-07-strings/labs/remove-all-nonalpha-chars.py
+++ lesson-07-strings/labs/remove-all-nonalpha-chars.py
@@ -1,1 +1,23 @@
+# write a program that removes all non-alpha chars from input
+a = input()             # input: -Hello, 1 world$!
 
+b = a.replace('-', '')  # removes - from original string
+print(b)                # Hello, 1 world$!
+c = b.replace(',', '')
+print(c)                # Hello 1 world$!
+d = c.replace('1 ', '')
+print(d)                # Hello world$!
+e = d.replace('$', '')
+print(e)                # Hello world!
+f = e.replace('!', '')
+print(f)
+
+# program can be simplified / less redundant by using the following:
+phrase = input()
+
+result = ''.join([char for char in phrase if char.isalpha()])
+# for char in phrase looks at each character one by one
+# if char.isalpha() will keep *only* alpha chars
+# char for char in phrase with the if statement makes a new list with only letters
+# finally, all characters are joined together to make one string
+print(result)
