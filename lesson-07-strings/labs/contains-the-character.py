fbb0763f9248b08748b7c89296f5ba32a20e493e
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-28 20:51:20 -0400

    Add solution

--- lesson-07-strings/labs/contains-the-character.py
+++ lesson-07-strings/labs/contains-the-character.py
@@ -1,1 +1,13 @@
+# write a program that reads a char, then a list of words
+# output is every word in the list that contains the char at least once
+# assume at least one word in the list will contain the given char
 
+letter = input()                # input: z
+words = input()                 # input: hello zoo sleep drizzle
+
+word_list = words.split()
+# print(word)                   # ['hello', 'zoo', 'sleep', 'drizzle']
+
+for w in word_list:
+    if letter in w:
+        print(w + ',', end='')  # output format: zoo,drizzle
