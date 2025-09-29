63357ee53d993fb406ec38c4864d0d6c28e29f4d
Oliver <67529552+oliverbebber@users.noreply.github.com>
2025-09-28 21:29:26 -0400

    Adding template to solve later

--- practice-test-1/car-wash.py
+++ practice-test-1/car-wash.py
@@ -1,1 +1,23 @@
+# write a program to calculate the total price for car wash services
+# a base wash is $10
+# a dictionary with each additional service and the corresponding cost
+# has been provided
+# two additional services can be selected
+# a '-' signifies an additional service was not selected
+# output all selected services, according to the input order, along with
+# the corresponding costs and then the total price for all car wash services
+services = { 'Air freshener' : 1 , 'Rain repellent': 2, 'Tire shine' : 2, 'Wax' : 3, 'Vacuum' : 5 }
+base_wash = 10
+total = 0
 
+service_choice1 = input()
+service_choice2 = input()
+
+
+# output format:
+  # ZyCar Wash
+  # Base car wash - $10
+  # Tire shine - $2
+  # Wax - $3
+  # -----
+  # Total price: $15
